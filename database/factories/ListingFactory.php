dfefe4b0390fa2cd876611f46261ad50dc1714ab
Jared <jaredcanonigo@gmail.com>
2022-09-11 02:19:42 -0700

    implemented styling template from Traversy Media

---
+++ database/factories/ListingFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ListingFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'id' => 000,
+            'title' => 'Title',
+            'tags' =>  'tag1,tag2,tag3',
+            'location' => $this->faker->city(),
+            'email' => $this->faker->companyEmail(),
+            'description' => $this->faker->paragraph(5)
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(5)->create();
+
+        // Listing::create([
+        //     'id' => '495023',
+        //     'title' => 'Movers needed!',
+        //     'tags' =>  'labor,cash',
+        //     'location' => 'La Costa, CA',
+        //     'email' => 'johndoe@gmail.com',
+        //     'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        //     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco'
+        // ]);
+        Listing::factory(5)->create();
     }
 }
