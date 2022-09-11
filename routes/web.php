514bc209bc33bc8f16a8e79db12115800f0b384e
Jared <jaredcanonigo@gmail.com>
2022-09-11 00:31:28 -0700

    finished sql migration

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Listings;
+use App\Models\Listing;
 
 /*
 |--------------------------------------------------------------------------
