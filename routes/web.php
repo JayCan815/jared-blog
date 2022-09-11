cdbc72f920e5fc95dd36eba885f5bdeb6f4dc619
Jared <jaredcanonigo@gmail.com>
2022-09-11 00:30:38 -0700

    configured and finish SQL migration

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Listing;
+use App\Models\Listings;
 
 /*
 |--------------------------------------------------------------------------
